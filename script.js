b859339a103f862f576d05bf4bff20374c3bef00
Ahmed <ahmedmahmud1994@hotmail.com>
2024-06-24 05:31:19 +0100

    Portfolio - Nav Active States

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('nav ul li a');
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('nav ul li a');
 
-    function changeActiveClass() {
-        let index = sections.length;
+  function changeActiveClass() {
+    let index = sections.length;
 
-        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) { }
 
-        navLinks.forEach((link) => link.classList.remove('active'));
-        navLinks[index].classList.add('active');
-    }
+    navLinks.forEach((link) => link.classList.remove('active'));
+    navLinks[index].classList.add('active');
+  }
 
-    changeActiveClass();
-    window.addEventListener('scroll', changeActiveClass);
-
+  changeActiveClass();
+  window.addEventListener('scroll', changeActiveClass);
 });
 
-// Function to check scroll position and toggle 'active' class
+
+// Function to check scroll position and toggle 'active' class on back to top button
 function checkScroll() {
   const backToTop = document.querySelector('.back-to-top');
   const scrollPosition = window.scrollY || document.documentElement.scrollTop;
